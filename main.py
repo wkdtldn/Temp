705a11eea13f23de1c87f711ce56358a5f316a5c
wkdtldn <136140859+wkdtldn@users.noreply.github.com>
2024-03-10 15:43:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-link = 'https://tutorials.pytorch.kr/recipes/recipes/defining_a_neural_network.html'
+# 신경망 기초
+link1 = 'https://tutorials.pytorch.kr/recipes/recipes/defining_a_neural_network.html'
+
+# NLP 기초
+link2 = 'https://tutorials.pytorch.kr/intermediate/char_rnn_classification_tutorial.html'
